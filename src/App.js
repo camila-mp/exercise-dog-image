a6aed423bcef0838cc2741b8afbbc5ed6a93a6b9
camila-mp <ca.malvp@gmail.com>
2021-07-01 09:54:18 -0300

    resolve exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.fetchDog = this.fetchDog.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      dogImagePath: '',
+    };
+  }
+
+  async componentDidMount() {
+    const imgUrl = await this.fetchDog();
+    this.updateState(imgUrl);
+  }
+
+  async handleClick() {
+    const imgUrl = await this.fetchDog();
+    this.updateState(imgUrl);
+  }
+
+  async fetchDog() {
+    const makeDogRequest = await fetch('https://dog.ceo/api/breeds/image/random');
+    const dogResponse = await makeDogRequest.json();
+    const imgPath = dogResponse.message;
+    return imgPath;
+  }
+
+  updateState(url) {
+    this.setState((state) => ({ ...state, dogImagePath: url }));
+  }
+
+  render() {
+    const { dogImagePath } = this.state;
+    if (dogImagePath === '') {
+      return (
+        <div className="main">
+          <p>LOADING...</p>
+        </div>
+      );
+    }
+    return (
+      <div className="main">
+        <img className="dog-picture" src={ dogImagePath } alt="random-dog" />
+        <button
+          className="dog-button"
+          type="button"
+          onClick={ this.handleClick }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Get random dog!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="dog-button">
+        <button type="button">Get random dog!</button>
+      </div>
+    );
+  }
+}
+
+export default Button;
